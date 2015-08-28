a5047bd7f796575d79af5ba29b38fe6cd0afcdf8
wuhua988 <wuhua988@163.com>
2015-08-28 14:28:29 +0800

    updata test

--- RedisClientTest/main.cpp
+++ RedisClientTest/main.cpp
@@ -8,16 +8,20 @@ void InitPb();
 int main(int argc, char * argv[]) 
 {
     //Initialize utility classes
-	log_init(LL_DEBUG, "redisApp", "./");
-    OS::Initialize();
-    OSThread::Initialize();
-    SocketUtils::Initialize(false);
+	log_init(LL_DEBUG, "log", "./");
 	
 	RedisPoolCluster rpl;
 	rpl.AddPool("127.0.0.1:6379",100);
 	RedisClient cli(&rpl);
-	bool ok = cli.Set("RedisTest","TestOK");
+	cli.set("RedisTest","TestOK");
 
+	string value;
+	if(cli.get("RedisTest",value))
+	{
+		LOG_DEBUG("value:%s",value.c_str());
+	}else{
+		LOG_DEBUG("Error");
+	}
     while(1)
     {
         sleep(1);
